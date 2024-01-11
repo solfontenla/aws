b565b61585ad02beb181264f035fc5ea6c05aaa8
solfontenla <sol.fontenla@nilus.co>
2024-01-11 11:14:29 -0300

    init rust project

---
+++ aws-app/target/debug/aws-app.d
@@ -0,0 +1,1 @@
+C:\Users\Nilus\aws\aws-app\target\debug\aws-app.exe: C:\Users\Nilus\aws\aws-app\src\main.rs
---
+++ aws-app/target/debug/deps/aws_app.d
@@ -0,0 +1,5 @@
+C:\Users\Nilus\aws\aws-app\target\debug\deps\aws_app.exe: src\main.rs
+
+C:\Users\Nilus\aws\aws-app\target\debug\deps\aws_app.d: src\main.rs
+
+src\main.rs:
